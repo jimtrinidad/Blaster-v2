5cd136b97ce62e3e2e300cd806c7f2456be6d50f
Jim <jim@Jim-Ubuntu-16.04.1>
2017-07-18 21:23:52 +0800

    completed autoloading, and clean old include file

--- Core/Loki.php
+++ Core/Loki.php
@@ -33,8 +33,6 @@ class Loki
 		//excute request routing
 		$this->router = new Router($this);
 
-		var_dump($this->router);
-
 		$module 		= $this->load_module();
 
 		if ($module !== false) {
@@ -94,8 +92,10 @@ class Loki
 
 			$controller_file = MODULE_DIR . $module . DS . $this->router->controller . '.php';
 
+			if (file_exists($controller_file)) {
+
 				$className 	= 'Loki\Module\\' . $module . '\\' . $this->router->controller;
-				echo $className;
+
 				if (class_exists($className)) {
 					return $className;
 				} else {
@@ -104,6 +104,13 @@ class Loki
 							'message'	=> 'Controller class not found.'
 						), true);
 				}
+
+			} else {
+				Response::show(array(
+						'status'	=> false,
+						'message'	=> 'Controller file not found.'
+					));
+			}
 
 		}
 
