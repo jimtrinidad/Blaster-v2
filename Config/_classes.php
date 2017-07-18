f8e0b7e0310c194fc1786ee8b64d6eac2f957910
jimtrinidad <jimtrinidad002@gmail.com>
2017-07-18 14:13:49 +0800

    try using autoload

--- Config/_classes.php
+++ Config/_classes.php
@@ -6,32 +6,38 @@
 set_include_path( implode( PATH_SEPARATOR, array(
 	get_include_path(),
 	SYSTEM_DIR,
-	VENDORS_DIR
+	VENDORS_DIR,
+	dirname(ROOT_DIR) . '/'
 )));
 
-/**
-* Core classes
-*/
-$core_class = array(
-	'Loki',
-	'Router',
-	'Response',
-	'Loader',
-	'Controller',
-	'Model',
-	'Utilities',
-	'Auth'
-);
+// /**
+// * Core classes
+// */
+// $core_class = array(
+// 	'Loki',
+// 	'Router',
+// 	'Response',
+// 	'Loader',
+// 	'Controller',
+// 	'Model',
+// 	'Utilities',
+// 	'Auth'
+// );
 
-/**
-* Additional 3rd party classes
-*/
-$third_parties	= array(
-	'MySQLi' => 'mysqli_db.class',
-);
+// /**
+// * Additional 3rd party classes
+// */
+// $third_parties	= array(
+// 	'MySQLi' => 'mysqli_db.class',
+// );
 
 
-$_classes = array_merge($third_parties, $core_class);
-foreach($_classes as $class) {
-	require_once $class . '.php';
-}
+// $_classes = array_merge($third_parties, $core_class);
+// foreach($_classes as $class) {
+// 	require_once $class . '.php';
+// }
+
+spl_autoload_register(function ($name) {
+    // var_dump(__NAMESPACE__, $name);
+    include str_replace('\\', '/', $name) . '.php';
+});
